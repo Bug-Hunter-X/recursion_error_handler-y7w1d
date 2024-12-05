025077148bd9c881b161ac867ce4c740cedbc5c5
BugHunter-X <askzenai@gmail.com>
2024-12-05 17:53:53 +0100

    Add bug.py file

---
+++ bug.py
@@ -0,0 +1,9 @@
+def my_function(x):
+    if x == 0:
+        return 0
+    else:
+        return my_function(x - 1) + 1
+
+print(my_function(5))  # Output: 5
+print(my_function(0))  # Output: 0
+print(my_function(-1)) # RecursionError: maximum recursion depth exceeded
